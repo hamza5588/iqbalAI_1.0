929f7dff7f593e62a3a792f35e5dd8f35c482eb4
MasterOz786 <i222434@nu.edu.pk>
2025-03-15 02:18:29 +0500

    deployment practices

--- app/utils/db.py
+++ app/utils/db.py
@@ -8,6 +8,7 @@ def get_db():
     """Get database connection."""
     if 'db' not in g:
         try:
+            print(current_app.config['DATABASE'])
             g.db = sqlite3.connect(
                 current_app.config['DATABASE'],
                 detect_types=sqlite3.PARSE_DECLTYPES
